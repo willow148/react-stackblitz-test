9cb6beb818ad9a1fa0d44a42ee3051f69e549baf
willow148 <yuzy90125@yahoo.co.jp>
2025-02-04 11:31:44 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some miracle happen !:)</p>
+      <p>Start editing to see some miracle happen !!!!!!!!:)</p>
     </div>
   );
 }
